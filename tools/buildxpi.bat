29de7df59728b59434578b97e61b305defe94c87
Yuan Xulei <xyuan@mozilla.com>
2011-12-30 12:49:21 +0800

    add build tool

---
+++ tools/buildxpi.bat
@@ -0,0 +1,12 @@
+set XPI_NAME=muter.xpi 
+cd ..
+del /f/q %XPI_NAME%
+cd extension
+..\tools\7za a ..\%XPI_NAME% chrome\
+..\tools\7za a ..\%XPI_NAME% components\
+..\tools\7za a ..\%XPI_NAME% defaults\
+..\tools\7za a ..\%XPI_NAME% plugins\npiemode.dll
+..\tools\7za a ..\%XPI_NAME% modules\*.jsm
+..\tools\7za a ..\%XPI_NAME% modules\ctypes-binary\*.dll
+..\tools\7za a ..\%XPI_NAME% chrome.manifest
+..\tools\7za a ..\%XPI_NAME% install.rdf
