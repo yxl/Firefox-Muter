41cada744e016e8a699b1e5603fa3657dadc57a6
Yuan Xulei <xyuan@mozilla.com>
2012-01-13 22:11:35 +0800

    merge

--- extension/defaults/preferences/prefs.js
+++ extension/defaults/preferences/prefs.js
@@ -3,7 +3,7 @@ pref('extensions.firefox-muter.shortcut.key', 'M');
 pref('extensions.firefox-muter.shortcut.modifiers', 'control alt');
 pref('extensions.firefox-muter.showInAddonBar', true);
 pref('extensions.firefox-muter.showInStatusBar', false);
-pref('extensions.firefox-muter.skin.updateurl', 'http://feedback.firefox.com.cn/muter/update.json');
+pref('extensions.firefox-muter.skin.updateurl', 'http://feedback.firefox.com.cn/muter/skin.json');
 pref('extensions.firefox-muter.disabledIcon', 'chrome://muter/skin/mute-disabled.png');
 pref('extensions.firefox-muter.enabledIcon', 'chrome://muter/skin/mute-enabled.png');
 pref('extensions.firefox-muter.switchButtonType', 'menu-button');
