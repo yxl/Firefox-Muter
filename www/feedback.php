0f8b74fbddeafd329c898bf82d3c5f4be9b7eaff
Yuan Xulei <yxl@fireie.org>
2013-05-23 02:07:29 +0000

    Send html email report instead of xml report.

--- www/feedback.php
+++ www/feedback.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+﻿<xml!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -6,7 +6,7 @@
 <body>
 <?php
     // Get the feedback xml content
-    $xml = file_get_contents('php://input');
+    $text = file_get_contents('php://input');
 
     // Compose an email with the feedback content
     $to = "xyuan@mozilla.com";  
@@ -14,14 +14,14 @@
     $from = "admin@yxl.name";
     $headers = "";
     $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/xml; charset=utf-8"."\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8"."\r\n";
     $headers .= "From: ".$from."\r\n";
     $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
     $date = date("Y-M-j h:i:s A");
-    $body = base64_encode($xml);
+    $body = $text;
     
     // Send the email 
-    if($xml) {
+    if($text) {
       if(@mail($to,$subject,$body,$headers)){
         echo("Succeeded!");
       }else{
